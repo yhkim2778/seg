7181a886513c7d887e643e77af163280f7f9d237
Younghan Kim <yhkim2778@gmail.com>
2018-11-26 03:21:12 +0900

    cc

--- convolutional_autoencoder.py
+++ convolutional_autoencoder.py
@@ -181,7 +181,7 @@ def draw_results(test_inputs, test_targets, test_segmentation, test_accuracy, ne
     fig, axs = plt.subplots(4, n_examples_to_plot, figsize=(n_examples_to_plot * 3, 10))
     fig.suptitle("Accuracy: {}, {}".format(test_accuracy, network.description), fontsize=20)
     for example_i in range(n_examples_to_plot):
-        axs[0][example_i].imshow(test_inputs[example_i], cmap='gray')
+        axs[0][example_i].imshow(test_inputs[example_i])
         axs[1][example_i].imshow(test_targets[example_i].astype(np.float32), cmap='gray')
         axs[2][example_i].imshow(
             np.reshape(test_segmentation[example_i], [network.IMAGE_HEIGHT, network.IMAGE_WIDTH]),
